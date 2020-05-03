0beb4c78fe4bc4b4dc54855384a5def5080c1fb9
hammmmm <61265522+hammmmm@users.noreply.github.com>
2020-05-03 13:27:06 +0900

    search window

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,10 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def search
+    @posts = Post.joins(:country).where('country LIKE ?', "#{params[:country]}")
+  end
+
   private
   def post_params
     params.require(:post).permit(:image, :category_id, :country_id).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts, except: :show
+  resources :posts, except: :show do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: :show
   resources :categories, only: :show
 end
