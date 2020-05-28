85ed90ef120dfd78c9d0e9589855e896ca01dcdf
hammmmm <61265522+hammmmm@users.noreply.github.com>
2020-05-28 20:07:24 +0900

    ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,20 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
   def create
-    like = current_user.likes.build(post_id: params[:post_id])
-    like.save
+    @like = current_user.likes.build(post_id: @post.id)
+    @like.save
     redirect_to root_path
   end
 
   def destroy
-    like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
-    like.destroy
+    @like = Like.find_by(post_id: @post.id, user_id: current_user.id)
+    @like.destroy
     redirect_to root_path
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
