fa6d9fcebcafacbcd1c97d94df50ee0b14e14026
hammmmm <61265522+hammmmm@users.noreply.github.com>
2020-05-29 13:09:06 +0900

    edit likes page
    投稿日降順に並び替え

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.page(params[:page]).per(16)
-    @like_posts = @user.like_posts.page(params[:page]).per(16)
+    @like_posts = @user.like_posts.order("created_at DESC").page(params[:page]).per(16)
   end
 end
